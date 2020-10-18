1443714d9eab12086292066fe54b9a2b86d12120
karimtounsi <karim.tounsi100@gmail.com>
2020-10-18 17:33:05 +0200

    Entity Category

--- src/main/java/pl/coderslab/charity/category/Category.java
+++ src/main/java/pl/coderslab/charity/category/Category.java
@@ -1,2 +1,25 @@
-package pl.coderslab.charity.category;public class Category {
+package pl.coderslab.charity.category;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
+
+import javax.persistence.*;
+import javax.transaction.Transactional;
+
+@Entity
+@Transactional
+@Getter
+@Setter
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "category_id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
 }
